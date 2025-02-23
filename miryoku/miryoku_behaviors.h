4946bce897b9604ccc396f110866def62c424ef9
braian-bernatto <braian5353@gmail.com>
2025-02-23 10:36:09 -0300

    Update miryoku_behaviors.h

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -3,10 +3,15 @@
 
 #pragma once
 
-// Define key positions for Corne
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // Left-hand keys
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // Right-hand keys
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // Thumb keys
+// Define key positions for Corne (42-key layout)
+// Left-hand keys (0-4, 12-16, 24-28)
+#define KEYS_L 0 1 2 3 4 12 13 14 15 16 24 25 26 27 28
+
+// Right-hand keys (6-11, 18-23, 30-35)
+#define KEYS_R 6 7 8 9 10 11 18 19 20 21 22 23 30 31 32 33 34 35
+
+// Thumb keys (36-41)
+#define THUMBS 36 37 38 39 40 41
 
 /* Left-hand HRMs */
 ZMK_HOLD_TAP(hml,
