02563e2b9796c5c9a640d547133d8245e461cd6a
braian-bernatto <braian5353@gmail.com>
2025-02-23 09:09:22 -0300

    Update miryoku_behaviors.h

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -3,5 +3,13 @@
 
 #pragma once
 
-#define U_MT(MOD, TAP) &u_mt MOD TAP
+// Define left and right hand home row mods
+#define U_MT_L(MOD, TAP) &lhm MOD TAP  // Left hand home row mods
+#define U_MT_R(MOD, TAP) &rhm MOD TAP  // Right hand home row mods
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
+// Redefine U_MT based on key position
+// This macro will automatically choose left or right behavior
+#define U_MT(MOD, TAP) \
+    (((TAP) <= 5 || ((TAP) >= 12 && (TAP) <= 17) || ((TAP) >= 24 && (TAP) <= 29) || ((TAP) >= 36 && (TAP) <= 38)) ? \
+    U_MT_L(MOD, TAP) : U_MT_R(MOD, TAP))
