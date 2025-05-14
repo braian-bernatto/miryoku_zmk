2dd7b9784a5e74eda929c57fcffe630ef0db4729
braian-bernatto <braian5353@gmail.com>
2025-05-13 23:08:01 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-# Enable increased transmit power
-CONFIG_BT_CTLR_TX_PWR_PLUS_8=y
-
-# Disable 2M PHY
-CONFIG_BT_CTLR_PHY_2M=n
